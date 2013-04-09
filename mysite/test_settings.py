80c78739f4f63cea37683b479e9d9f66ef15ecc8
Daniel Rus Morales <danirus@eml.cc>
2013-04-09 10:06:01 +0200

    Adds django_coverage to report on buildbot step

--- mysite/test_settings.py
+++ mysite/test_settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'polls',
+    'django_coverage',
 )
 
 # A sample logging configuration. The only tangible logging
