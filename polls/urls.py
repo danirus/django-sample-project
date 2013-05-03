a80ace90c85fa72dbbc267444839ebc3650db2fd
Daniel Rus Morales <danirus@eml.cc>
2013-05-03 10:26:34 +0200

    Better names for polls app urls

--- polls/urls.py
+++ polls/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='polls-index'),
     # ex: /polls/5/
-    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<poll_id>\d+)/$', views.detail, name='polls-detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='polls-results'),
     # ex: /polls/5/vote/
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='polls-vote'),
 )
