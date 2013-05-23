dfc791f3cc010199a6816ab3ce8dfb174d169281
Daniel Rus Morales <danirus@eml.cc>
2013-05-23 22:03:31 +0200

    Removes dependency on django-coverage to avoid errors under python3

--- mysite/test_settings.py
+++ mysite/test_settings.py
@@ -125,7 +125,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'polls',
-    'django_coverage',
 )
 
 # A sample logging configuration. The only tangible logging
